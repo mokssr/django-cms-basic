6acd967cca9308780c8a68085e7196cb35248580
Sukma Raharja <sukmaraharja@outlook.co.id>
2020-09-01 02:44:55 +0800

    Initial commit: - setup a new django project - add basic form for product input - add basic page to display product list

---
+++ shop/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ProductForm(forms.Form):
+    name = forms.CharField(label='Nama Produk', max_length=120, required=True)
+    slug = forms.SlugField(label='Link produk', max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, label='Deskripsi Produk')
+    price = forms.IntegerField(label="Harga Produk", min_value=1000)
+    stock = forms.IntegerField(label="Jumlah Stok", min_value=0)
---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price  = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
+    # image
