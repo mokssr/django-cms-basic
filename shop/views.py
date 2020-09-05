40eda283191c9690a6bb19f24a1c16374f3617f6
Sukma Raharja <sukmaraharja@outlook.co.id>
2020-09-05 19:10:50 +0800

    remove jsonresponse

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.urls import reverse
